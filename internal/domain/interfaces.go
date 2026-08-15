1c38c8eb76f91172619735cb6a07379a42ff4777
CaueTech <cauemine10@gmail.com>
2026-08-15 18:16:55 -0300

    feat: added new interface for future Kafka consumer and producer modules

--- internal/domain/interfaces.go
+++ internal/domain/interfaces.go
@@ -1,5 +1,7 @@
 package domain
 
+import "context"
+
 // Validator é responsável por validar e fazer o parse dos dados brutos recebidos
 type Validator interface {
 	Validate(rawPayload []byte) (*Event, error)
@@ -10,4 +12,16 @@ type Aggregator interface {
 	// Process recebe um evento válido. Retorna um *Alert caso a regra seja disparada,
 	// ou nil caso o evento seja apenas processado sem gerar alertas.
 	Process(event *Event) (*Alert, error)
+}
+
+type MessageProducer interface {
+    PublishAlert(ctx context.Context, alert Alert) error
+    PublishDLQ(ctx context.Context, dlq DLQEnvelope) error
+    Close() error
+}
+
+type MessageConsumer interface {
+    // Recebe o channel para onde deve enviar os bytes brutos
+    StartConsuming(ctx context.Context, ingestionChan chan<- []byte) error
+    Close() error
 }
